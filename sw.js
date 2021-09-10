6ee61f5af3fccd99041cf2f71a3dc1e32718ba6d
o.vaquero <ovaquero@indaba.lks.es>
2021-09-10 10:44:41 +0200

    Modificamos el APP_SHELL del  SW

--- sw.js
+++ sw.js
@@ -7,16 +7,16 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
     // '/',
-    'index.html',
-    'css/style.css',
-    'img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    'img/avatars/ironman.jpg',
-    'img/avatars/spiderman.jpg',
-    'img/avatars/thor.jpg',
-    'img/avatars/wolverine.jpg',
-    'js/app.js',
-    'js/sw-utils.js'
+    './index.html',
+    './css/style.css',
+    './img/favicon.ico',
+    './img/avatars/hulk.jpg',
+    './img/avatars/ironman.jpg',
+    './img/avatars/spiderman.jpg',
+    './img/avatars/thor.jpg',
+    './img/avatars/wolverine.jpg',
+    './js/app.js',
+    './js/sw-utils.js'
 ];
 
 // Lo que no se va a modificar nunca 
